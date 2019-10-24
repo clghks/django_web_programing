94aaf70ec9bb9f490ba076cb1a5e8a125bfb8216
chihwan <clghks@gmail.com>
2019-10-24 18:10:31 +0900

    설문조사 앱 만들기 - 화면 생성

--- polls/polls/views.py
+++ polls/polls/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("투표 목록 화면입니다.")
+
+def detail(request, question_id):
+    return HttpResponse("투표 화면입니다.")
+
+def results(request, question_id):
+    return HttpResponse("투표 결과 화면입니다.")
