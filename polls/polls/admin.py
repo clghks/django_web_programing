ca1ee7be12bab23916d37c676b90b6090479f767
chihwan <clghks@gmail.com>
2019-10-24 21:15:28 +0900

    관리자 페이지에 설문조사 모델 등록 - Choice 등록

--- polls/polls/admin.py
+++ polls/polls/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+   model = Choice
+   extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+   inlines = [ChoiceInline]
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
