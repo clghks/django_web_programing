873fb16985660c370f00c0e7c82ab87b15b6c206
chihwan <clghks@gmail.com>
2019-10-26 20:44:45 +0900

    bookmark list 화면 출력

--- bookmark/bookmark/views.py
+++ bookmark/bookmark/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Bookmark
 
 # Create your views here.
+class BookmarkListView(ListView):
+    model = Bookmark
