099b1b03024bf7428b84bdbc68b6c0892fe158c7
chihwan <clghks@gmail.com>
2019-10-26 21:12:44 +0900

    Bookmark 삭제 기능 구현

--- bookmark/bookmark/views.py
+++ bookmark/bookmark/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from .models import Bookmark
@@ -21,3 +21,7 @@ class BookmarkUpdateView(UpdateView):
     model = Bookmark
     fields = ['site_name', 'url']
     template_name_suffix = '_update'
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('list')
