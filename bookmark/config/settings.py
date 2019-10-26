6307cddcbaf6364d1de7b26b0775e79b6941a3ea
chihwan <clghks@gmail.com>
2019-10-26 21:57:15 +0900

    배포환경 설정

--- bookmark/config/settings.py
+++ bookmark/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '296c*=z*md+5h#8+8ozmt*7$2rnxg^yn=ad2orb7)@^my62md_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
